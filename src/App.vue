8683daceedec8ef60c1959bc10be4616610ba4a4
Rudy Quevedo <rquevedo@NWE-H064H5RQ235.local>
2018-06-21 18:43:44 -0700

    vue-cli installed

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+    <div class="coming-soon">
+      <p>Status: just getting started first deploy 6/21/18</p>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>REFACTORING.LIFE</h1>
+    <h3>Changing my life and documenting it</h3>
+    <h6>-Rudy Quevedo</h6>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+<style lang="scss">
+.home{
+  height: 100vh;
+  position: relative;
+  h1, h3, h6{
+    margin:0px;
+  }
+}
+.coming-soon{
+  position: fixed;
+  bottom: 0px;
+  height: 20px;
+  width: 97%;
+  background: #0E1E25;
+  color: #c4c4c4;
+  text-align: center;
+  p{
+    margin:0px;
+  }
+}
+</style>
